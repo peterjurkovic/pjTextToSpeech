2164520d4b87d8b4c64aff476e21f53e07b9feb1
peterjurkovic <petojurkovic@gmail.com>
2015-04-26 00:46:40 +0100

    First release

--- app.js
+++ app.js
@@ -7,26 +7,23 @@ app.controller('TTSCtrl', function( $scope, TTS_EVENTS, $log, TTSConfig){
 
     console.log(TTSConfig);
 
+    TTSConfig.url = 'http://tts.peterjurkovic.com/tts-backend/index.php';
+
     $scope.tts = {
         text : 'Hello word',
         lang : 'en'
     };
 
     $scope.langList =  [
-        {"code":"hr","name":"Croatian","nativeName":"hrvatski"},
-        {"code":"cs","name":"Czech","nativeName":"česky, čeština"},
-        {"code":"da","name":"Danish","nativeName":"dansk"},
-        {"code":"nl","name":"Dutch","nativeName":"Nederlands, Vlaams"},
         {"code":"en","name":"English","nativeName":"English"},
-        {"code":"et","name":"Estonian","nativeName":"eesti, eesti keel"},
+        {"code":"cs","name":"Czech","nativeName":"česky, čeština"},
+        {"code":"nl","name":"Dutch","nativeName":"Nederlands, Vlaams"},
         {"code":"fi","name":"Finnish","nativeName":"suomi, suomen kieli"},
         {"code":"fr","name":"French","nativeName":"français, langue française"},
         {"code":"de","name":"German","nativeName":"Deutsch"},
-        {"code":"el","name":"Greek, Modern","nativeName":"Ελληνικά"},
         {"code":"it","name":"Italian","nativeName":"Italiano"},
         {"code":"pl","name":"Polish","nativeName":"polski"},
         {"code":"sk","name":"Slovak","nativeName":"slovenčina"},
-        {"code":"sl","name":"Slovene","nativeName":"slovenščina"},
         {"code":"es","name":"Spanish","nativeName":"español, castellano"},
         {"code":"sv","name":"Swedish","nativeName":"svenska"}
     ];
---
+++ tts-backend/index.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'Text2Speach.php';
+	
+header("Expires: 0");
+header('Content-Type: application/json');
+
+if(isset($_GET['text']) && strlen($_GET['text']) > 2 && isset($_GET['lang']) && strlen($_GET['lang']) == 2){
+    $tts = new Text2Speach();
+    try{
+    	$filePath = $tts->getFilePath($_GET['text'], $_GET['lang']);
+    	setHeaders(200);
+    	return toJson(array( "path" => getDomainName() . $filePath ));
+    }catch(Exception $e){}
+  }
+setHeaders(400);
+return toJson(array( "error" => "Invalid request, check required params [text] and [lang]" ));
+ 
+  
+
+function toJson($data){
+	echo json_encode($data);
+}
+
+function setHeaders($code){
+	header("{$_SERVER['SERVER_PROTOCOL']} $code");
+}
+
+
+function getDomainName(){
+	return (
+			$_SERVER['REMOTE_ADDR'] == '127.0.0.1' ?
+			'http://tts.dev' : 'http://tts.peterjurkovic.com'
+			).'/tts-backend';
+}
